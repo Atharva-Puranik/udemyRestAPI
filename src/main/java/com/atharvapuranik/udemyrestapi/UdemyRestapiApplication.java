0d616da865af373c28c44967f3f741ea3bb16450
Atharva-Puranik <atharvap887@gmail.com>
2023-11-30 11:52:53 +0530

    Project Upload

---
+++ src/main/java/com/atharvapuranik/udemyrestapi/UdemyRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.atharvapuranik.udemyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atharvapuranik/udemyrestapi/UdemyRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atharvapuranik.udemyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
